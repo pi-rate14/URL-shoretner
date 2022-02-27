3fedf530fb9415203b6858e290316f2839f66520
pi-rate14 <apoorva.srivastava2019@vitstudent.ac.in>
2022-02-27 20:13:12 +0530

    URL validation

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,11 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/pi-rate14/url-shortener/helpers"
+)
 
 
 
@@ -16,4 +21,29 @@ type response struct {
 	Expiry			time.Duration	`json:"expiry"`
 	XRateRemaining	int				`json:"rate_limit"`
 	XRateLimitReset	time.Duration	`json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"cannot parse JSON"})
+	}
+
+	// rate limiting
+
+	// check to see if input is a valid URL
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Innvalid URL"})
+	}
+
+	// check for domain error
+
+	if !helpers.RemoveDomainError(body.URL){
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error":"you cannot access this domain"})
+	}
+
+	// https, SSL
+
+	body.URL = helpers.EnforeceHTTP(body.URL)
 }
